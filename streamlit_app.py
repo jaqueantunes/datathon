4a524da93a231633a4b9496822af02ddf4561268
jaqueantunes <61162356+jaqueantunes@users.noreply.github.com>
2026-02-21 16:41:14 -0300

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,29 @@
+import streamlit as st
+import joblib
+import numpy as np
+
+modelo = joblib.load("modelo_risco.pkl")
+imputer = joblib.load("imputer.pkl")
+
+st.title("🎓 Previsão de Risco de Defasagem")
+
+st.markdown("Modelo preditivo baseado nos indicadores educacionais da Passos Mágicos.")
+
+ida = st.number_input("IDA", 0.0, 10.0, 6.0)
+ieg = st.number_input("IEG", 0.0, 10.0, 7.0)
+iaa = st.number_input("IAA", 0.0, 10.0, 7.0)
+ips = st.number_input("IPS", 0.0, 10.0, 7.0)
+ipv = st.number_input("IPV", 0.0, 10.0, 7.0)
+
+if st.button("Prever"):
+    dados = np.array([[ida, ieg, iaa, ips, ipv]])
+    dados = imputer.transform(dados)
+    
+    prob = modelo.predict_proba(dados)[0][1]
+    
+    st.subheader(f"Probabilidade de Risco: {prob:.2%}")
+    
+    if prob > 0.5:
+        st.error("⚠️ Alto risco de defasagem.")
+    else:
+        st.success("✅ Baixo risco.")
